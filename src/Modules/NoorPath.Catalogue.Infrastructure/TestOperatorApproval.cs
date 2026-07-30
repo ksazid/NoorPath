6f72d8b1647efc82c594dd82e2d265bd77a89f04
Sazid <47315271+ksazid@users.noreply.github.com>
2026-07-30 13:45:19 +0200

    refactor: remove obsolete S02 operator approval helper

--- src/Modules/NoorPath.Catalogue.Infrastructure/TestOperatorApproval.cs
+++
@@ -1,9 +0,0 @@
-using NoorPath.Catalogue;
-
-namespace NoorPath.Catalogue.Infrastructure;
-
-public sealed class TestOperatorApproval(IEnumerable<string> operatorIds) : IOperatorApproval
-{
-    private readonly HashSet<string> approved = operatorIds.ToHashSet(StringComparer.Ordinal);
-    public bool IsApprovedTestOperator(string operatorId) => approved.Contains(operatorId);
-}
