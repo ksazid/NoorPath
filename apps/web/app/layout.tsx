3f1cd7a1debda325f881b4db9cd85e43ae8daf32
Sazid <47315271+ksazid@users.noreply.github.com>
2026-08-04 09:53:23 +0200

    VS-18: load remaining shared primitive styles

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -4,6 +4,7 @@ import "@noorpath/design-tokens/tokens.css";
 import FamilyQuoteBridge from "./FamilyQuoteBridge";
 import "./styles.css";
 import "./design-system.css";
+import "./primitives.css";
 import "./shells.css";
 import "./composer.css";
 import "./commercial.css";
